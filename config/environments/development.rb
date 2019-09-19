97acae9f079b30626ef479b6b6b1292c10806372
Kthom1 <keenan.thompson67@gmail.com>
2019-09-19 23:52:47 +0800

    Set reset password email to deliver_now; It now works properly with letter opener; Made localhost:3000 the default action mailer url in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,5 +64,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   # Set default host for mailer
-  # config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
