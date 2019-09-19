c2a30ab87558d9d762ed35a6cb1a6034ca30def2
Kthom1 <keenan.thompson67@gmail.com>
2019-09-20 01:09:49 +0800

    Added method to user model to make sure that reset token is used in a valid timeframe; Added reset token time validation logic to passwords#update; Refactored passwords controller, with before_actions

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,14 +1,16 @@
 class PasswordsController < ApplicationController
+  before_action :find_user_by_reset_password_token, only: [:update]
+  before_action :find_user_by_email, only: [:create]
+  
   def new
     
   end
 
   def create
-    user = User.find_by(email: params[:email])
     if params[:email].strip.empty?
       redirect_to '/forgot', flash: {:messages => ["Please enter an email address."]}
-    elsif user.present?
-      UserMailer.reset_password_email(user).deliver_now
+    elsif @user.present?
+      UserMailer.reset_password_email(@user).deliver_now
       redirect_to '/forgot', flash: {:messages => ["Nice, we'll send you an email with instructions."]}
     else
       redirect_to '/forgot',flash: {:messages => ["Can't find that email address."]}
@@ -21,9 +23,8 @@ class PasswordsController < ApplicationController
 
   def update
     error_array = check_for_errors(params) 
-    if error_array.empty?
-      user = User.find_by(reset_password_token: params[:token])
-      user.reset_password(params[:password])
+    if error_array.empty? && @user.reset_password_token_valid?
+      @user.reset_password(params[:password])
       redirect_to login_path, flash: {:messages => ["You successfully updated your password."]}
     else
       redirect_to reset_url(token: params[:token]), flash: {:messages => error_array}
@@ -35,6 +36,16 @@ class PasswordsController < ApplicationController
     if params[:password].strip.length < 8 then error_array.push("Password is too short (minimum is 8 characters)") end
     if params[:password] != params[:password_confirmation] then error_array.push("Password confirmation doesn't match Password.") end
     return error_array
+  end
+
+  private
+
+  def find_user_by_reset_password_token
+    @user = User.find_by(reset_password_token: params[:token])
+  end
+
+  def find_user_by_email
+    @user = User.find_by(email: params[:email])
   end
 
 end
