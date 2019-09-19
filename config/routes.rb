40a44cb9e67d12c25168dfaf2705510ba35cdd6d
Kthom1 <keenan.thompson67@gmail.com>
2019-09-20 01:17:38 +0800

    Ran rails d controller pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#index' 
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
@@ -15,4 +14,6 @@ Rails.application.routes.draw do
   post 'forgot' => 'passwords#create'
   get 'reset' => 'passwords#edit'
   patch 'reset' => 'passwords#update'
+
+  root to: 'users#new' 
 end
