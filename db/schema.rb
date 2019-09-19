d8c5ed4c825041becdfc876f546b23cc33c0aadf
Kthom1 <keenan.thompson67@gmail.com>
2019-09-19 20:06:34 +0800

    Generated migration to add password reset columns to user; Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_083812) do
+ActiveRecord::Schema.define(version: 2019_09_19_120453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_09_18_083812) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
 end
