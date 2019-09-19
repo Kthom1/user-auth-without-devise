754ed771e63a12372c0a9982da92aa3fe98f1dcd
Kthom1 <keenan.thompson67@gmail.com>
2019-09-19 23:07:25 +0800

    Implemented basic password reset token email; Added methods to the user model to generate and return tokens; Implemented basic logic in passwords controller; Created appropriate password reset mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: @user.email, subject: 'Welcome to Our Site')
   end
+
+  def reset_password_email(user)
+    @url = reset_url(token: user.generate_reset_password_token)
+    mail(to: user.email, subject: 'Reset Password')
+  end
 end
