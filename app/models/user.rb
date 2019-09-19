37f2d675dcf02c1986da419b2e0e335752a88253
Kthom1 <keenan.thompson67@gmail.com>
2019-09-19 18:56:15 +0800

    Added custom param cleaning to remove blank passwords from update requests, and check password length on update request

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :password, length: { minimum: 8 }
+  validates :password, length: { minimum: 8 }, on: :create
   validates :name, length: {minimum: 5}, on: :update
   validates :email, uniqueness: true, on: :create
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
@@ -13,6 +13,17 @@ class User < ApplicationRecord
     user = User.new(user_create_params)
     user.name = first_username = return_username(user_create_params[:email])
     return user
+  end
+
+  def self.clean_update_params(user_update_params)
+    if user_update_params[:password].strip.empty?
+      cleaned_params = user_update_params.except(:password, :password_confirmation)
+      return {:valid_params => cleaned_params}
+    elsif user_update_params[:password].length < 8
+      return {:error => " is too short (minimum is 8 characters)"}
+    else 
+      return {:valid_params => user_update_params}
+    end
   end
 
 end
