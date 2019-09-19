119db2170019fb43623ea88d07702e57d556e1ce
Kthom1 <keenan.thompson67@gmail.com>
2019-09-20 01:31:46 +0800

    Changed users#show to users#create because it was more appropriate; Now roots to users#new; Refactored sessions controller, adding a before action and error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,27 @@
 class SessionsController < ApplicationController
+  before_action :find_user_by_email, only: [:create]
+
   def new
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to '/'
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/profile'
     else
-      redirect_to '/login'
+      redirect_to '/login', flash: {:messages => ["Please try again."]}
     end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
+  end
+
+  private
+
+  def find_user_by_email
+    @user = User.find_by(email: params[:email])
   end
 
 end
