930ab7df7847302180b53073e15ddab430161112
Kthom1 <keenan.thompson67@gmail.com>
2019-09-21 02:09:42 +0800

    Added capybara-email gem to test emails; Reworded severval test scenarios and refactored tests improving readability; Added tests to make sure the reset password email is sent to the correct email address, has the correct content, and that clicking the link inside properly redirects the user to the reset password page

--- spec/features/passwords/forgot_password_spec.rb
+++ spec/features/passwords/forgot_password_spec.rb
@@ -1,16 +1,26 @@
 require "rails_helper"
 
-RSpec.feature "Reset password", :type => :feature do
+RSpec.feature "Forgot password", :type => :feature do
   let(:user) { create(:user) }
 
-  scenario 'user navigates to the forgot page and successfully gets a reset password email', js: true do
-    user
-    visit root_path 
-    click_on 'Forgot Password'
+  background do
+    visit forgot_path 
+  end
+
+  scenario 'user successfully requests a reset password email', js: true do
     within('form') do
       fill_in 'Email', with: user.email
     end
     click_on 'Reset Password'
     expect(page).to have_content('Nice, we\'ll send you an email with instructions.')
   end
+
+  scenario 'user fails to request a reset password email because the user\'s email is not registered', js: true do
+    within('form') do
+      fill_in 'Email', with: 'bob@bob.com'
+    end
+    click_on 'Reset Password'
+    expect(page).to have_content('Can\'t find that email address.')
+  end
 end
+
--- spec/features/users/sign_up_spec.rb
+++ spec/features/users/sign_up_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "Sign up", :type => :feature do
-
-  scenario 'user navigates to the root path and successfully signs up', js: true do
+  background do
     visit root_path 
+  end
+
+  scenario 'user successfully signs up', js: true do
     within('form') do
       fill_in 'Email', with: 'bob@bob.com'
       fill_in 'Password', with: '12345678'
@@ -13,8 +15,7 @@ RSpec.feature "Sign up", :type => :feature do
     expect(page).to have_content('Profile')
   end
 
-  scenario 'user navigates to the root path and fails to sign up because his password is too short', js: true do
-    visit root_path 
+  scenario 'user fails to sign up because his password is too short', js: true do
     within('form') do
       fill_in 'Email', with: 'bob@bob.com'
       fill_in 'Password', with: '1234567'
@@ -24,8 +25,7 @@ RSpec.feature "Sign up", :type => :feature do
     expect(page).to have_content('Password is too short (minimum is 8 characters)')
   end
 
-  scenario 'user navigates to the root path and fails to sign up because his email is invalid', js: true do
-    visit root_path 
+  scenario 'user fails to sign up because his email is invalid', js: true do
     within('form') do
       fill_in 'Email', with: 'bob'
       fill_in 'Password', with: '12345678'
@@ -35,8 +35,7 @@ RSpec.feature "Sign up", :type => :feature do
     expect(page).to have_content('Email is invalid')
   end
 
-  scenario 'user navigates to the root path and fails to sign up because his password does not match his password confirmation', js: true do
-    visit root_path 
+  scenario 'user fails to sign up because his password does not match his password confirmation', js: true do
     within('form') do
       fill_in 'Email', with: 'bob@email.com'
       fill_in 'Password', with: '12345678'
@@ -45,7 +44,6 @@ RSpec.feature "Sign up", :type => :feature do
     click_on 'Submit'
     expect(page).to have_content('Password confirmation doesn\'t match Password')
   end
-
 end
 
 
