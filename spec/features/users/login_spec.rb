e2d084f66690b17069165a0c20faa523a5532275
Kthom1 <keenan.thompson67@gmail.com>
2019-09-21 02:09:42 +0800

    Added capybara-email gem to test emails; Reworded severval test scenarios and refactored tests improving readability; Added tests to make sure the reset password email is sent to the correct email address, has the correct content, and that clicking the link inside properly redirects the user to the reset password page

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 RSpec.feature "Login", :type => :feature do
   let(:user) { create(:user) }
 
-  scenario 'user navigates to the login page and successfully logs in', js: true do
-    user
-    visit root_path 
-    click_on 'Login'
+  background do
+    visit login_path
+  end
+
+  scenario 'existing user successfully logs in', js: true do
     within('form') do
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
@@ -15,10 +16,7 @@ RSpec.feature "Login", :type => :feature do
     expect(page).to have_content('Profile')
   end
 
-  scenario 'user navigates to the login page and fails to login because user account does not exist', js: true do
-    user
-    visit root_path 
-    click_on 'Login'
+  scenario 'user fails to login because user account does not exist', js: true do
     within('form') do
       fill_in 'Email', with: 'bob@bob.com'
       fill_in 'Password', with: '12345678'
@@ -26,5 +24,4 @@ RSpec.feature "Login", :type => :feature do
     click_on 'Submit'
     expect(page).to have_content('Please try again.')
   end
-
 end
---
+++ spec/mailers/reset_password_email_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "Reset password mailer", :type => :feature do
+  let(:user) { create(:user) }
+
+  background do
+    clear_emails
+    visit forgot_path
+    within('form') do
+      fill_in 'Email', with: user.email
+    end
+    click_on 'Reset Password'    
+    open_email(user.email)
+  end
+
+  scenario 'reset password email has the correct address', js: true do
+    expect(current_email.to).to eq [user.email]
+  end
+
+  scenario 'reset password email has the correct content', js: true do
+    expect(current_email).to have_content('Password Reset Instructions')
+    clear_emails
+  end
+
+  scenario 'user clicks reset password link in email and is successfully redirected to the reset password page', js: true do
+    current_email.click_link 'Reset Password!'
+    expect(page).to have_content 'Enter a new password'
+  end
+end
