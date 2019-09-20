f10348887c5f8c4d929ad5bfaf3b63e1285cb41d
Kthom1 <keenan.thompson67@gmail.com>
2019-09-21 06:24:22 +0800

    Added several tests to show user model validations are working as expected

--- spec/model/user.rb
+++ spec/model/user.rb
@@ -1,14 +1,29 @@
 require "rails_helper"
 
-context 'User model validations' do
+context 'User model validation' do
   let(:user) { build(:user) }
 
-  it 'creates succesfully' do 
+  it 'user is created successfully with proper parameters' do 
     expect(user).to be_valid
   end
 
-  it 'invalid because name shorter than 5 characters on update' do 
+  it 'update is valid because name has at least 5 characters' do 
+    user.update(name: 'a' * 5)
+    expect(user).to be_valid
+  end
+
+  it 'update is invalid because name shorter than 5 characters' do 
     user.update(name: 'a')
+    expect(user).not_to be_valid
+  end
+
+  it 'update is valid because password has at least 8 characters' do 
+    user.update(password: 'a' * 8, password_confirmation: 'a' * 8)
+    expect(user).to be_valid
+  end
+
+   it 'update is invalid because password has fewer than 8 characters' do 
+    user.update(password: 'a' * 7, password_confirmation: 'a' * 7)
     expect(user).not_to be_valid
   end
 end
