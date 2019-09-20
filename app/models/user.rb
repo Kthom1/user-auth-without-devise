6b7eca1184f14f297a84fa67b12721573340f9aa
Kthom1 <keenan.thompson67@gmail.com>
2019-09-21 05:20:26 +0800

    Added separate password length validations for create and update in user model; The update validation now accepts blank; Refactored users controller and model, removing methods that are no longer needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 8 }, on: :create
+  validates :password, length: { minimum: 8 }, on: :update, allow_blank: true
   validates :name, length: {minimum: 5}, on: :update
   validates :email, uniqueness: true, on: :create
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
 
   def self.return_username(email)
     email.split("@")[0]
@@ -13,17 +15,6 @@ class User < ApplicationRecord
     user = User.new(user_create_params)
     user.name = first_username = return_username(user_create_params[:email])
     return user
-  end
-
-  def self.clean_update_params(user_update_params)
-    if user_update_params[:password].strip.empty?
-      cleaned_params = user_update_params.except(:password, :password_confirmation)
-      return {:valid_params => cleaned_params}
-    elsif user_update_params[:password].length < 8
-      return {:error => " is too short (minimum is 8 characters)"}
-    else 
-      return {:valid_params => user_update_params}
-    end
   end
 
   def generate_reset_password_token
@@ -47,5 +38,4 @@ class User < ApplicationRecord
   def generate_token
     SecureRandom.hex(10)
   end
-
 end
